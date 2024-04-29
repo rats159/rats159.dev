1a4b546d41ea57732af04ed21700080c77d0b7aa
rats159 <113955400+rats159@users.noreply.github.com>
2024-04-28 22:22:12 -0500

    up indentation also site work

--- Lynx.js
+++ Lynx.js
@@ -8,217 +8,235 @@
   so i wouldnt reccomend it
 */
 export class ElementChain {
-  constructor() {
-    this.nodes = [];
-  }
+    constructor() {
+        this.nodes = [];
+    }
 
-  _elem(
-    attributes = new AttrChain(),
-    children = new ElementChain(),
-    name = "div"
-  ) {
-    const element = document.createElement(name);
-    this.nodes.push(attributes.apply(element));
-    element.append(...children.render());
-    return this;
-  }
+    _elem(
+        attributes = new AttrChain(),
+        children = new ElementChain(),
+        name = "div"
+    ) {
+        const element = document.createElement(name);
+        this.nodes.push(attributes.apply(element));
+        element.append(...children.render());
+        return this;
+    }
 
-  render() {
-    return this.nodes;
-  }
+    render() {
+        return this.nodes;
+    }
 
-  Component(chain) {
-    this.nodes.push(chain().render());
-    return this;
-  }
+    Component(elem) {
+        this.nodes.push(elem().render()[0]);
+        return this;
+    }
 
-  Many(...components) {
-    components.forEach((component) => this.nodes.push(component().render()));
-    return this;
-  }
+    Many(...components) {
+        components.forEach((component) =>
+            this.nodes.push(component().render()[0])
+        );
+        return this;
+    }
 
-  Text(txt) {
-    this.nodes.push(new Text(txt));
-    return this;
-  }
+    Text(txt) {
+        this.nodes.push(new Text(txt));
+        return this;
+    }
 
-  Header({ attributes, children } = {}) {
-    return this._elem(attributes, children, "header");
-  }
+    Header({ attributes, children } = {}) {
+        return this._elem(attributes, children, "header");
+    }
 
-  Nav({ attributes, children } = {}) {
-    return this._elem(attributes, children, "nav");
-  }
+    Nav({ attributes, children } = {}) {
+        return this._elem(attributes, children, "nav");
+    }
 
-  A({ attributes, children } = {}) {
-    return this._elem(attributes, children, "a");
-  }
+    A({ attributes, children } = {}) {
+        return this._elem(attributes, children, "a");
+    }
 
-  H1({ attributes, children } = {}) {
-    return this._elem(attributes, children, "h1");
-  }
+    H1({ attributes, children } = {}) {
+        return this._elem(attributes, children, "h1");
+    }
 
-  H2({ attributes, children } = {}) {
-    return this._elem(attributes, children, "h2");
-  }
+    H2({ attributes, children } = {}) {
+        return this._elem(attributes, children, "h2");
+    }
 
-  H3({ attributes, children } = {}) {
-    return this._elem(attributes, children, "h3");
-  }
+    H3({ attributes, children } = {}) {
+        return this._elem(attributes, children, "h3");
+    }
 
-  H4({ attributes, children } = {}) {
-    return this._elem(attributes, children, "h4");
-  }
+    H4({ attributes, children } = {}) {
+        return this._elem(attributes, children, "h4");
+    }
 
-  H5({ attributes, children } = {}) {
-    return this._elem(attributes, children, "h5");
-  }
+    H5({ attributes, children } = {}) {
+        return this._elem(attributes, children, "h5");
+    }
 
-  H6({ attributes, children } = {}) {
-    return this._elem(attributes, children, "h6");
-  }
+    H6({ attributes, children } = {}) {
+        return this._elem(attributes, children, "h6");
+    }
 
-  Button({ attributes, children } = {}) {
-    return this._elem(attributes, children, "button");
-  }
+    Ul({ attributes, children } = {}) {
+        return this._elem(attributes, children, "ul");
+    }
 
-  P({ attributes, children } = {}) {
-    return this._elem(attributes, children, "p");
-  }
+    Ol({ attributes, children } = {}) {
+        return this._elem(attributes, children, "ol");
+    }
 
-  Span({ attributes, children } = {}) {
-    return this._elem(attributes, children, "span");
-  }
+    Li({ attributes, children } = {}) {
+        return this._elem(attributes, children, "li");
+    }
 
-  Main({ attributes, children } = {}) {
-    return this._elem(attributes, children, "main");
-  }
-  Section({ attributes, children } = {}) {
-    return this._elem(attributes, children, "section");
-  }
+    Button({ attributes, children } = {}) {
+        return this._elem(attributes, children, "button");
+    }
+
+    P({ attributes, children } = {}) {
+        return this._elem(attributes, children, "p");
+    }
+
+    Span({ attributes, children } = {}) {
+        return this._elem(attributes, children, "span");
+    }
+
+    Main({ attributes, children } = {}) {
+        return this._elem(attributes, children, "main");
+    }
+    Section({ attributes, children } = {}) {
+        return this._elem(attributes, children, "section");
+    }
 }
 
 export class AttrChain {
-  constructor() {
-    this.nodes = [];
-  }
-  apply(elem) {
-    for (const attrNode of this.nodes) {
-      switch (attrNode.type) {
-        case "immediate":
-          attrNode.value(elem);
-          break;
-        case "classlist":
-          elem.classList.add(...attrNode.value);
-          break;
-        case "onclick":
-          console.log(attrNode.value);
-          elem.addEventListener("click", attrNode.value.bind(null, elem));
-          break;
-        default:
-          elem[attrNode.type] = attrNode.value;
-          break;
-      }
+    constructor() {
+        this.nodes = [];
     }
-    return elem;
-  }
+    apply(elem) {
+        for (const attrNode of this.nodes) {
+            switch (attrNode.type) {
+                case "immediate":
+                    attrNode.value(elem);
+                    break;
+                case "classlist":
+                    elem.classList.add(...attrNode.value);
+                    break;
+                case "onclick":
+                    console.log(attrNode.value);
+                    elem.addEventListener(
+                        "click",
+                        attrNode.value.bind(null, elem)
+                    );
+                    break;
+                default:
+                    elem[attrNode.type] = attrNode.value;
+                    break;
+            }
+        }
+        return elem;
+    }
 
-  Immediate(fn) {
-    this.nodes.push({ type: "immediate", value: fn });
-    return this;
-  }
+    Immediate(fn) {
+        this.nodes.push({ type: "immediate", value: fn });
+        return this;
+    }
 
-  Class(...classes) {
-    this.nodes.push({ type: "classlist", value: classes });
-    return this;
-  }
+    Class(...classes) {
+        this.nodes.push({ type: "classlist", value: classes });
+        return this;
+    }
 
-  ID(id) {
-    this.nodes.push({ type: "id", value: id });
-    return this;
-  }
+    ID(id) {
+        this.nodes.push({ type: "id", value: id });
+        return this;
+    }
 
-  Text(value) {
-    this.nodes.push({ type: "textContent", value });
-    return this;
-  }
+    Text(value) {
+        this.nodes.push({ type: "textContent", value });
+        return this;
+    }
 
-  OnClick(value) {
-    this.nodes.push({ type: "onclick", value });
-    return this;
-  }
+    OnClick(value) {
+        this.nodes.push({ type: "onclick", value });
+        return this;
+    }
 
-  HREF(value) {
-    this.nodes.push({ type: "href", value });
-    return this;
-  }
+    HREF(value) {
+        this.nodes.push({ type: "href", value });
+        return this;
+    }
 
-  OnLoad(value) {
-    this.nodes.push({ type: "onload", value });
-    return this;
-  }
+    OnLoad(value) {
+        this.nodes.push({ type: "onload", value });
+        return this;
+    }
 }
 
 export class Page extends ElementChain {
-  render() {
-    for (const element of this.nodes) {
-      document.body.appendChild(element);
+    render() {
+        for (const element of this.nodes) {
+            document.body.appendChild(element);
+        }
     }
-  }
 }
 
 export class Solo {
-  constructor({
-    attr = new AttrChain(),
-    children = new ElementChain(),
-    element = "div",
-  }) {
-    this.attr = attr;
-    this.children = children;
-    this.element = element;
-    console.log(...children.render());
-  }
+    constructor({
+        attributes = new AttrChain(),
+        children = new ElementChain(),
+        element = "div",
+    }) {
+        this.attributes = attributes;
+        this.children = children;
+        this.element = element;
+    }
 
-  render() {
-    const element = this.attr.apply(document.createElement(this.element));
-    element.append(...this.children.render());
-    return [element];
-  }
+    render() {
+        const element = this.attributes.apply(
+            document.createElement(this.element)
+        );
+        element.append(...this.children.render());
+        return [element];
+    }
 }
 
 export class Signal {
-  constructor(initialValue) {
-    this.value = initialValue;
-    this.effects = [];
-    this.alwaysEffects = [];
-  }
-
-  get() {
-    return this.value;
-  }
-
-  set(value) {
-    let dirty = true;
-    if (this.value == value) {
-      dirty = false;
-    }
-    this.value = value;
-    if (dirty) {
-      for (const effect of this.effects) {
-        effect(this);
-      }
+    constructor(initialValue) {
+        this.value = initialValue;
+        this.effects = [];
+        this.alwaysEffects = [];
     }
 
-    for (const effect of this.alwaysEffects) {
-      effect(this);
+    get() {
+        return this.value;
     }
-  }
 
-  addEffect(fn, always) {
-    if (always) {
-      this.alwaysEffects.push(fn);
-    } else {
-      this.effects.push(fn);
+    set(value) {
+        let dirty = true;
+        if (this.value == value) {
+            dirty = false;
+        }
+        this.value = value;
+        if (dirty) {
+            for (const effect of this.effects) {
+                effect(this);
+            }
+        }
+
+        for (const effect of this.alwaysEffects) {
+            effect(this);
+        }
     }
-  }
+
+    addEffect(fn, always) {
+        if (always) {
+            this.alwaysEffects.push(fn);
+        } else {
+            this.effects.push(fn);
+        }
+    }
 }
--- script.js
+++ script.js
@@ -1,79 +1,128 @@
 import * as Lynx from "./Lynx.js";
 import { typeout } from "./typeout.js";
 
-function Section() {}
+function Comment(text) {
+    return () => {
+        return new Lynx.Solo({
+            element: "p",
+            attributes: new Lynx.AttrChain().Class("comment").Text(text),
+        });
+    };
+}
+
+function Link(href, text) {
+    return () => {
+        return new Lynx.Solo({
+            attributes: new Lynx.AttrChain()
+                .HREF(href)
+                .Text(text)
+                .Class("link"),
+            element: "a",
+        });
+    };
+}
+
+function ListItem(text) {
+    return () => {
+        return new Lynx.Solo({
+            element: "li",
+            attributes: new Lynx.AttrChain().Text(text),
+        });
+    };
+}
+
+function List(...items) {
+    return () => {
+        return new Lynx.Solo({
+            element: "ul",
+            children: new Lynx.ElementChain().Many(
+                ...items.map((item) => {
+                    return ListItem(item);
+                })
+            ),
+        });
+    };
+}
+
+function Section(id, title, content) {
+    return () => {
+        return new Lynx.Solo({
+            element: "section",
+            attributes: new Lynx.AttrChain().ID(id),
+            children: new Lynx.Solo({
+                element: "article",
+                children: new Lynx.ElementChain()
+                    .H2({
+                        attributes: new Lynx.AttrChain().Text(title),
+                    })
+                    .Component(() => content),
+            }),
+        });
+    };
+}
 
 new Lynx.Page()
-  .Header({
-    children: new Lynx.ElementChain()
-      .H1({
-        attributes: new Lynx.AttrChain()
-          .Text("Hello, World!")
-          .Immediate((element) => {
-            console.log("hello?");
-            typeout("Hello, World!", 100, (text) => {
-              element.textContent = text;
-              console.log(text);
-            });
-          }),
-      })
-      .Nav({
+    .Header({
         children: new Lynx.ElementChain()
-          .A({
-            attributes: new Lynx.AttrChain()
-              .Text("About me")
-              .HREF("#about")
-              .Class("link"),
-          })
-          .A({
-            attributes: new Lynx.AttrChain()
-              .Text("My projects")
-              .HREF("#projects")
-              .Class("link"),
-          })
-          .A({
-            attributes: new Lynx.AttrChain()
-              .Text("About the site")
-              .HREF("#site")
-              .Class("link"),
-          }),
-      }),
-  })
-  .Main({
-    children: new Lynx.ElementChain().Section({
-      attributes: new Lynx.AttrChain().ID("about"),
-      children: new Lynx.Solo({
-        element: "article",
-        children: new Lynx.ElementChain()
-          .H2({
-            attributes: new Lynx.AttrChain().Text("About me:"),
-          })
-          .P({
-            children: new Lynx.ElementChain()
-              .Text(
-                `Most people call me rats. If you have something else to call me, you already know that. I've been programming for around ${
-                  new Date().getFullYear() - 2020
-                } years now. For more about what I've done with that time, check out `
-              )
-              .A({
+            .H1({
                 attributes: new Lynx.AttrChain()
-                  .HREF("#projects")
-                  .Text("my projects")
-                  .Class("link"),
-              })
-              .Text("."),
-          })
-          .P({
-            attributes: new Lynx.AttrChain()
-              .Class("comment")
-              .Text("//TODO: Expand this"),
-          })
-          .P({
-            attributes: new Lynx.AttrChain()
-              .Class("comment")
-              .Text("//TODO: Start caring about TODOs"),
-          }),
-      }),
-    }),
-  })
-  .render();
+                    .Text("Hello, World!")
+                    .Immediate((element) => {
+                        typeout("Hello, World!", 100, (text) => {
+                            element.textContent = text;
+                        });
+                    }),
+            })
+            .Nav({
+                children: new Lynx.ElementChain()
+                    .Component(Link("#about", "About me"))
+                    .Component(Link("#projects", "My projects"))
+                    .Component(Link("#site", "About the site")),
+            }),
+    })
+    .Main({
+        children: new Lynx.ElementChain().Component(
+            Section(
+                "about",
+                "About Me:",
+                new Lynx.Solo({
+                    element: "p",
+                    children: new Lynx.ElementChain()
+                        .Text(
+                            `Most people call me rats. I've been programming for around `
+                        )
+                        .Span({
+                            attributes: new Lynx.AttrChain()
+                                .Text(new Date().getFullYear() - 2020)
+                                .Class("special", "mono"),
+                        })
+                        .Text(
+                            ` years now. For more about what I've done with that time, check out `
+                        )
+
+                        .Component(Link("#projects", "my projects"))
+                        .Text(
+                            ". I'm mainly a Java and web dev, but I have at least some experience in all of the following languages:"
+                        )
+                        .Component(
+                            List(
+                                "Java",
+                                "The web trio (css,js,html)",
+                                "C",
+                                "Rust"
+                            )
+                        )
+                        .Component(Comment("//TODO: Expand this"))
+                        .Component(Comment("//TODO: Start caring about TODOs")),
+                    // .Ul({
+                    //     children: new Lynx.ElementChain().Li({
+                    //         attributes: new Lynx.AttrChain().Text("Java"),
+                    //     }),
+                    // }),
+                })
+            )
+        ),
+        // .Component(Section("projects", "My Projects:"))
+        // .Component(Section("site", "About the Site:")),
+    })
+    .render();
